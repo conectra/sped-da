2e8cd263d1e7c6489806b25ba177b2f586d247d7
petry <petry@conectra.net.br>
2018-01-30 15:44:15 -0200

    [ADD] - Adicionada exibição de peso

--- src/CTe/DacteV3.php
+++ src/CTe/DacteV3.php
@@ -1756,7 +1756,7 @@ class DacteV3 extends Common
             'size' => 5,
             'style' => '');
         $this->pTextBox($x+8, $y, $w, $h, $texto, $aFont, 'T', 'L', 0, '');
-        //$texto = $this->pSimpleGetValue($this->infQ->item(0), "qCarga") . "\r\n";
+        $qCarga = $this->pSimpleGetValue($this->infQ->item(0), "qCarga") . "\r\n";
         $texto = number_format($qCarga, 3,",",".")
             .' '.$this->pSimpleGetValue($this->infQ->item(0),"tpMed");
         //$texto .= ' ' . $this->zUnidade($this->pSimpleGetValue($this->infQ->item(0), "cUnid"));
@@ -1773,7 +1773,8 @@ class DacteV3 extends Common
             'size' => 5,
             'style' => '');
         $this->pTextBox($x+20, $y, $w, $h, $texto, $aFont, 'T', 'L', 0, '');
-        $texto = number_format(0, 3, ",", ".");
+        $qCarga = $this->pSimpleGetValue($this->infQ->item(0), "qCarga") . "\r\n";
+        $texto = number_format($qCarga, 3,",",".");
         $aFont = array(
             'font' => $this->fontePadrao,
             'size' => 7,
